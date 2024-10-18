31892f57129134fd3b541ae167298c533255fe50
Andres Castro <andrescastro2011@hotmail.com>
2024-10-18 11:08:32 -0300

    refactorizacion de styles SideBar y NavBar

--- app/client/components/Navbar/NavBar.tsx
+++ app/client/components/Navbar/NavBar.tsx
@@ -1,12 +1,13 @@
-
 import { Button } from 'primereact/button';
 import Image from 'next/image';
 
-export default function Navbar() {
+const Navbar: React.FC = () => {
   return (
-    <nav className="primary h-3rem p-2 flex align-items-center justify-content-between" style={{margin:0, padding:0}}>
-        <Image src="/logo.png" alt="Logo" width={40} height={40} />
-        <Button icon="pi pi-cog" />
+    <nav className="primary h-3rem p-2 flex align-items-center justify-content-between">
+      <Image className='ml-1' priority src="/logo.png" alt="Logo" width={40} height={40} />
+      <Button aria-label="Configuración" icon="pi pi-cog" />
     </nav>
   );
-}
+};
+
+export default Navbar;
--- app/client/components/SideBar/Sidebar.tsx
+++ app/client/components/SideBar/Sidebar.tsx
@@ -1,13 +1,16 @@
 
-export default function Sidebar() {
-    const icons = Array(6).fill('pi pi-box')
-    return(
-        <div style={{width:'60px'}} className="h-full bg-sidebar p-3">
-                <div className="flex flex-column gap-3">
+const Sidebar: React.FC = () => {
+    const icons = Array(6).fill('pi pi-box');
+    return (
+        <div className="max-w-4rem h-full bg-sidebar flex justify-content-center ">
+            <div className="mt-4 flex flex-column gap-3">
                 {icons.map((icon, index) => (
-                    <i key={index} className={`${icon} text-xl text-white`}  />
+                    <i key={index} className={`${icon} text-xl text-white`} />
                 ))}
             </div>
         </div>
-    )
-}
+
+    );
+};
+
+export default Sidebar;
